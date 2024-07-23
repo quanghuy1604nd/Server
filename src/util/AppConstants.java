1bb87984f477f5896fa6391f5d7923dd96ec7a4f
henrytran1604 <quanghuy1604nd@gmail.com>
2024-07-23 21:40:37 +0700

    update

--- src/util/AppConstants.java
+++ src/util/AppConstants.java
@@ -10,20 +10,13 @@ package util;
  */
 public class AppConstants {
     // Remote Server
-//    public static final String JDBC_URL = "jdbc:postgresql://db_container:5432/ltm_demo";
-//    public static final String WEBHOOK_LOG_ENDPOINT = "http://20.198.242.126:8080/api/webhook/log";
-//    public static final String WEBHOOK_SCOREBOARD_ENDPOINT = "http://20.198.242.126:8080/api/webhook/scoreboard";
-//    public static final String JDBC_URL = "jdbc:postgresql://db_container:5432/ltm_demo";
 
-    
-    // dev
-    public static final String WEBHOOK_LOG_ENDPOINT = "http://localhost:8080/api/webhook/log";
-    public static final String WEBHOOK_SCOREBOARD_ENDPOINT = "http://localhost:8080/api/webhook/scoreboard";
-    public static final String JDBC_URL = "jdbc:postgresql://localhost:5432/ltm_demo";
-
+    public static final String WEBHOOK_LOG_ENDPOINT = System.getenv().getOrDefault("WEBHOOK_LOG_ENDPOINT", "http://localhost:8080/api/webhook/log");
+    public static final String WEBHOOK_SCOREBOARD_ENDPOINT = System.getenv().getOrDefault("WEBHOOK_SCOREBOARD_ENDPOINT", "http://localhost:8080/api/webhook/scoreboard");
+    public static final String JDBC_URL = System.getenv().getOrDefault("JDBC_URL", "jdbc:postgresql://localhost:5432/ltm_demo");
+    public static final String JDBC_USERNAME = System.getenv().getOrDefault("JDBC_USERNAME", "postgres");
+    public static final String JDBC_PASSWORD = System.getenv().getOrDefault("JDBC_PASSWORD", "1234");
     public static final String CLASS_FOR_NAME = "org.postgresql.Driver";
-    public static final String JDBC_USERNAME = "postgres";
-    public static final String JDBC_PASSWORD = "1234";
 
 
     public static final int TIME_OUT_DURATION = 5000;
@@ -32,7 +25,7 @@ public class AppConstants {
     public static final int DATA_STREAM_PORT = 1605;
     public static final int CHARACTER_STREAM_PORT = 1606;
     public static final int OBJECT_STREAM_PORT = 1608;
-    
+
     public static final int MAX_CONNECTION = Math.max(2, Runtime.getRuntime().availableProcessors() - 2);
     public static final int MAX_CONNECTION_PER_MINUTE = 10;
     public static final int WAITING_TIME = 10;
@@ -45,6 +38,5 @@ public class AppConstants {
     public static final int REQUEST_WRONG_EXERCISE = 104;
     public static final int INVALID_USER = 105;
     public static final int FORBIDDEN = 106;
-
 
 }
